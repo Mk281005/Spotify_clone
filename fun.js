2c557f1d502df02f5d4862640150a712e5e81c2e
Mk281005 <manoj281005@gmail.com>
2024-10-14 14:55:01 +0530

    Update

---
+++ fun.js
@@ -0,0 +1,157 @@
+console.log("hi");
+let cur = new Audio();
+let song;
+function secondsToMinutesSeconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+    return "00:00";
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+async function getsongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let an = div.getElementsByTagName("a");
+  song = [];
+  for (let i = 0; i < an.length; i++) {
+    const ele = an[i];
+
+    if (ele.href.endsWith(".mp3")) {
+      song.push(ele.href.split("/songs/")[1]);
+    }
+  }
+
+  return song;
+}
+
+
+
+const playnow = (tra) => {
+  cur.src = "/songs/" + tra + "mp3";
+  let pl = document.getElementById("play");
+
+  pl.src = "pause.svg";
+  cur.play();
+  document.querySelector(".songinfo").innerHTML = tra.replace(".","");
+  document.querySelector(".songdur").innerHTML="00:00/00:00"
+ 
+  
+};
+
+
+
+async function main() {
+  let songs = await getsongs();
+  console.log(songs);
+
+  let playlist = document.getElementsByClassName("playlist")[0];
+  let ul = playlist.getElementsByTagName("ul")[0];
+let i=0;
+  for (const son of songs) {
+    let li = document.createElement("li");
+    let s = son.replaceAll("%20", " ").replaceAll("mp3", "");
+    li.innerHTML = ` 
+  <img src="music.svg" class="invert" alt="">
+  <div class="info">
+      <div class="in">${s}</div>
+  </div>
+  <div class="playnow">
+     
+      <img src="play2.svg" class="invert" alt="">
+  </div>`;
+    ul.appendChild(li);
+  }
+  Array.from(
+    document.querySelector(".playlist").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      playnow(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
+  let pl = document.getElementById("play");
+  pl.addEventListener("click", () => {
+    if (!cur.src) {
+      let firstSong = document
+        .querySelector(".playlist ul li .info .in")
+        .innerHTML.trim();
+      playnow(firstSong);
+    } else if (cur.paused) {
+      cur.play();
+      pl.src = "pause.svg";
+    } else {
+      cur.pause();
+      pl.src = "play2.svg";
+    }
+  });
+  cur.addEventListener("timeupdate", () => {
+    document.querySelector(".songdur").innerHTML=`${secondsToMinutesSeconds(cur.currentTime)}/ ${secondsToMinutesSeconds(cur.duration)}`;
+    document.querySelector(".seek").style.left = `${(cur.currentTime/cur.duration)*100}%`;
+    document.querySelector(".col").style.width = `${(cur.currentTime/cur.duration)*100}%`;
+  });
+
+ 
+  document.querySelector(".seekbar").addEventListener('click',e=>{
+      let per=(e.offsetX/e.target.getBoundingClientRect().width)*100;
+      document.querySelector(".seek").style.left = per+"%";
+      cur.currentTime=((cur.duration)*per)/100;
+  })}
+ document.querySelector("#previous").addEventListener("click", ()=>{
+  console.log("clicked");
+  if (!cur.src) {
+    let firstSong = document
+      .querySelector(".playlist ul li .info .in")
+      .innerHTML.trim();
+    playnow(firstSong);
+  } 
+  else{
+    let ind=cur.src.split("/songs/")[1];
+    let index=song.indexOf(ind);
+    if(index-1>0){
+      console.log(song[index-1].replaceAll("%20", " ").replaceAll("mp3", ""))
+      playnow(song[index-1].replaceAll("%20", " ").replaceAll("mp3", ""));
+    }
+    else{
+      console.log(song[song.length-1].replaceAll("%20", " ").replaceAll("mp3", ""));
+      playnow(song[song.length-1].replaceAll("%20", " ").replaceAll("mp3", ""));
+    }
+   
+  }
+  
+  
+ })
+ document.querySelector("#next").addEventListener("click", ()=>{
+  console.log("clicked");
+  if (!cur.src) {
+    let firstSong = document
+      .querySelector(".playlist ul li .info .in")
+      .innerHTML.trim();
+    playnow(firstSong);
+  } 
+  else{
+    let ind=cur.src.split("/songs/")[1];
+    let index=song.indexOf(ind);
+    if(index+1<song.length){
+      console.log(song[index+1].replaceAll("%20", " ").replaceAll("mp3", ""))
+      playnow(song[index+1].replaceAll("%20", " ").replaceAll("mp3", ""));
+    }
+    else{
+      console.log(song[0].replaceAll("%20", " ").replaceAll("mp3", ""));
+      playnow(song[0].replaceAll("%20", " ").replaceAll("mp3", ""));
+    }
+   
+  }
+  
+ })
+
+
+main();
